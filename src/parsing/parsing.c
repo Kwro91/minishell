e869df3cfbc4470ccc68d53ac9ca52fa8b41fba5
Benjamin SALORT <besalort@paul-f5Br4s5.clusters.42paris.fr>
2024-03-19 19:17:42 +0100

    Va falloir peut etre modifier split

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,21 +6,62 @@
 /*   By: besalort <besalort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:30:01 by besalort          #+#    #+#             */
-/*   Updated: 2024/03/19 16:51:24 by besalort         ###   ########.fr       */
+/*   Updated: 2024/03/19 19:11:01 by besalort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	parse_dquote(t_mdata *data, t_command *cmd, int i)
+int	*create_tab(t_mdata *data)
+{
+	int	*tab;
+	int	i;
+	
+	i = 0;
+	tab = malloc(sizeof(int) * 4096);
+	if (!tab)
+		ft_error(data, "Error: malloc\n", -1);
+	while (i < 4096)
+	{
+		tab[i] = -1;
+		i++;
+	}
+	return (tab);
+}
+
+void	add_tab(int *tab, int value)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4096 && tab[i] != -1)
+		i++;
+	tab[i] = value;
+}
+
+void	update_tab(int	*tab, int j)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4096 && tab[i])
+	{
+		if (i >= j)
+			tab[i] -= 1;
+		i++;
+	}
+}
+
+int	parse_dquote(t_mdata *data, t_command *cmd, int i, int *tab)
 {
 	int	len;
 
+	add_tab(tab, i - 1);
 	len = ft_strlen(cmd->line);
 	while (i < len && cmd->line)
 	{
 		if (cmd->line[i] == '"')
-			return (i);
+			return (add_tab(tab, i), i);
 		else if (cmd->line[i] == '$')
 			i = handle_dollar(data, cmd, i);
 		i++;
@@ -30,15 +71,16 @@ int	parse_dquote(t_mdata *data, t_command *cmd, int i)
 	return (ft_error(data, "Error: double quote not closed\n", 2), -2);
 }
 
-int	parse_squote(t_mdata *data, t_command *cmd, int i)
+int	parse_squote(t_mdata *data, t_command *cmd, int i, int *tab)
 {
 	int	len;
 
+	add_tab(tab, i - 1);
 	len = ft_strlen(cmd->line);
 	while (i < len && cmd->line)
 	{
 		if (cmd->line[i] == '\'')
-			return (i);
+			return (add_tab(tab, i), i);
 		i++;
 		len = ft_strlen(cmd->line);
 	}
@@ -46,21 +88,81 @@ int	parse_squote(t_mdata *data, t_command *cmd, int i)
 	return (ft_error(data, "Error: simple quote not closed\n", 2), -2);
 }
 
-void	remove_all_quotes(t_mdata *data, t_command *cmd)
+void	remove_char(t_mdata *data, t_command *cmd, int i)
 {
-	char	**tab;
+	char	*one;
+	char	*two;
 
-	tab = cmd->cmd;
-	if (!cmd || !*tab)
-		return ;
-	while (*tab)
+	one = NULL;
+	two = NULL;
+	one = ft_strndup(data, cmd->line, i);
+	if (cmd->line[i + 1])
 	{
-		remove_quotes(data, tab);
-		tab++;
+		two = ft_strdup(&cmd->line[i + 1]);
+		if (!two)
+			return (ft_error(data, "Error: strdup\n", -1));
+	}
+	if (one && two)
+	{
+		ft_free_me(cmd->line);
+		cmd->line = ft_strjoin(one, two);
+		if (!cmd->line)
+			return (ft_error(data, "Error: strjoin\n", -1));
+		ft_free_me(one);
+		ft_free_me(two);
+		return ;
+	}
+	else if (one && !two)
+	{
+		ft_free_me(cmd->line);
+		cmd->line = ft_strdup(one);
+		ft_free_me(one);
+		if (!cmd->line)
+			return (ft_error(data, "Error: strdup\n", -1));
+	}
+	else if (!one && two)
+	{
+		ft_free_me(cmd->line);
+		cmd->line = ft_strdup(two);
+		ft_free_me(two);
+		if (!cmd->line)
+			return (ft_error(data, "Error: strdup\n", -1));
 	}
 }
 
-void	parse_norm(t_mdata *data, t_command *cmd)
+void	remove_all_quotes(t_mdata *data, t_command *cmd, int *tab)
+{
+	// char	**tab;
+
+	// tab = cmd->cmd;
+	// if (!cmd || !*tab)
+	// while (*tab)
+	// {
+		// remove_quotes(data, tab);
+		// tab++;
+	// }
+	// remove_quotes(data, &cmd->line);
+	int	i;
+
+	i = 0;
+	while(i < 4096 && tab[i] >= 0)
+	{
+		// printf("line:%s:\n", cmd->line);
+		// printf("char a retirer :cmd->line[%i]=[%c]:\n", tab[i], cmd->line[tab[i]]);
+		remove_char(data, cmd, tab[i]);
+		update_tab(tab, i);
+		i++;
+	}
+	i = 0;
+	printf("line:%s:\n", cmd->line);
+	while (i < 4096 && tab[i] > -1)
+	{
+		printf("On splitera a :cmd->line[%i]=%c:\n", tab[i] + 1, cmd->line[tab[i] + 1]);
+		i++;
+	}
+}
+
+void	parse_norm(t_mdata *data, t_command *cmd, int *tab)
 {
 	char	*cmp;
 
@@ -71,17 +173,20 @@ void	parse_norm(t_mdata *data, t_command *cmd)
 		return ;
 	}
 	cmp = " \t";
+	remove_all_quotes(data, cmd, tab);
 	cmd->cmd = split_cmd(data, cmd->line, cmp);
-	remove_all_quotes(data, cmd);
 }
 
 void	parse_cmd(t_mdata *data, t_command *cmd)
 {
 	int		i;
+	int		*tab;
+	
 	int		len;
 
 	i = 0;
-	printf("line:%s:\n", cmd->line);
+	tab = create_tab(data);
+	// printf("line:%s:\n", cmd->line);
 	if (!cmd->line)
 	{
 		cmd->good = -1;
@@ -91,9 +196,9 @@ void	parse_cmd(t_mdata *data, t_command *cmd)
 	while (i >= 0 && cmd->line && i < len)
 	{
 		if (cmd->line[i] == '"')
-			i = parse_dquote(data, cmd, i + 1);
+			i = parse_dquote(data, cmd, i + 1, tab);
 		else if (cmd->line[i] == '\'')
-			i = parse_squote(data, cmd, i + 1);
+			i = parse_squote(data, cmd, i + 1, tab);
 		else if (cmd->line[i] == '$')
 			i = handle_dollar(data, cmd, i);
 		if (i < 0)
@@ -104,5 +209,6 @@ void	parse_cmd(t_mdata *data, t_command *cmd)
 		i++;
 		len = ft_strlen(cmd->line);
 	}
-	parse_norm(data, cmd);
+	parse_norm(data, cmd, tab);
+	free(tab);
 }
--- src/parsing/remove_quote.c
+++ src/parsing/remove_quote.c
@@ -6,102 +6,102 @@
 /*   By: besalort <besalort@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 18:36:45 by besalort          #+#    #+#             */
-/*   Updated: 2024/03/07 13:17:59 by besalort         ###   ########.fr       */
+/*   Updated: 2024/03/19 18:53:19 by besalort         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	parse_quotes(t_mdata *data, char *line, int i, char c)
-{
-	int	len;
+// int	parse_quotes(t_mdata *data, char *line, int i, char c)
+// {
+// 	int	len;
 
-	(void)data;
-	len = ft_strlen(line);
-	while (i < len && line)
-	{
-		if (line[i] == c)
-			return (i);
-		i++;
-		len = ft_strlen(line);
-	}
-	return (-2);
-}
+// 	(void)data;
+// 	len = ft_strlen(line);
+// 	while (i < len && line)
+// 	{
+// 		if (line[i] == c)
+// 			return (i);
+// 		i++;
+// 		len = ft_strlen(line);
+// 	}
+// 	return (-2);
+// }
 
-int	check_empty_quotes(t_mdata *data, char **line, int i)
-{
-	char	*new;
+// int	check_empty_quotes(t_mdata *data, char **line, int i)
+// {
+// 	char	*new;
 
-	new = NULL;
-	if (*line && &line[0][i] == &line[0][i + 1])
-	{
-		if (&line[0][i + 2])
-		{
-			new = ft_strdup(&line[0][i + 2]);
-			if (!line)
-				return (ft_error(data, "Error: remove quote1\n", -1), 1);
-		}
-		else
-		{
-			new = ft_strdup("");
-			if (!line)
-				return (ft_error(data, "Error: remove quote1\n", -1), 1);
-		}
-		*line = new;
-		return (1);
-	}
-	return (0);
-}
+// 	new = NULL;
+// 	if (*line && &line[0][i] == &line[0][i + 1])
+// 	{
+// 		if (&line[0][i + 2])
+// 		{
+// 			new = ft_strdup(&line[0][i + 2]);
+// 			if (!line)
+// 				return (ft_error(data, "Error: remove quote1\n", -1), 1);
+// 		}
+// 		else
+// 		{
+// 			new = ft_strdup("");
+// 			if (!line)
+// 				return (ft_error(data, "Error: remove quote1\n", -1), 1);
+// 		}
+// 		*line = new;
+// 		return (1);
+// 	}
+// 	return (0);
+// }
 
-int	remove_dquote(t_mdata *data, char **line, int i)
-{
-	if (check_empty_quotes(data, line, i) == 1)
-		return (i);
-	if (parse_quotes(data, *line, i + 1, '"') == -2)
-		return (i + 1);
-	get_removed(data, line, i);
-	while (*line && line[0][i])
-	{
-		if (line[0][i] == '"')
-			return (get_removed(data, line, i), i);
-		i++;
-	}
-	return (i);
-}
+// int	remove_dquote(t_mdata *data, char **line, int i)
+// {
+// 	if (check_empty_quotes(data, line, i) == 1)
+// 		return (i);
+// 	if (parse_quotes(data, *line, i + 1, '"') == -2)
+// 		return (i + 1);
+// 	get_removed(data, line, i);
+// 	while (*line && line[0][i])
+// 	{
+// 		if (line[0][i] == '"')
+// 			return (get_removed(data, line, i), i);
+// 		i++;
+// 	}
+// 	return (i);
+// }
 
-int	remove_squote(t_mdata *data, char **line, int i)
-{
-	if (check_empty_quotes(data, line, i) == 1)
-		return (i);
-	if (parse_quotes(data, *line, i + 1, '\'') == -2)
-		return (i + 1);
-	get_removed(data, line, i);
-	while (*line && line[0][i])
-	{
-		if (line[0][i] == '\'')
-			return (get_removed(data, line, i), i);
-		i++;
-	}
-	return (i);
-}
+// int	remove_squote(t_mdata *data, char **line, int i)
+// {
+// 	if (check_empty_quotes(data, line, i) == 1)
+// 		return (i);
+// 	if (parse_quotes(data, *line, i + 1, '\'') == -2)
+// 		return (i + 1);
+// 	get_removed(data, line, i);
+// 	while (*line && line[0][i])
+// 	{
+// 		if (line[0][i] == '\'')
+// 			return (get_removed(data, line, i), i);
+// 		i++;
+// 	}
+// 	return (i);
+// }
 
-void	remove_quotes(t_mdata *data, char **line)
-{
-	int	i;
-	int	len;
+// void	remove_quotes(t_mdata *data, char **line)
+// {
+// 	int	i;
+// 	int	len;
 
-	i = 0;
-	if (!line || !line[0])
-		return ;
-	len = ft_strlen(line[0]);
-	while (i < len)
-	{
-		if (line[0][i] && line[0][i] == '"')
-			i = remove_dquote(data, line, i);
-		else if (line[0][i] && line[0][i] == '\'')
-			i = remove_squote(data, line, i);
-		else
-			i++;
-		len = ft_strlen(line[0]);
-	}
-}
+// 	i = 0;
+// 	if (!line || !line[0])
+// 		return ;
+// 	len = ft_strlen(line[0]);
+// 	while (i < len)
+// 	{
+// 		if (line[0][i] && line[0][i] == '"')
+// 			i = remove_dquote(data, line, i);
+// 		else if (line[0][i] && line[0][i] == '\'')
+// 			i = remove_squote(data, line, i);
+// 		else
+// 			i++;
+// 		len = ft_strlen(line[0]);
+// 	}
+// }
